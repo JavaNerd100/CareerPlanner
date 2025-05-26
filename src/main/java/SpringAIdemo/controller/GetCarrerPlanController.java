5018eafad983ea96e068ef4b149c224e887ab51e
Gourav Thakur <104557560+JavaNerd100@users.noreply.github.com>
2025-05-26 17:54:08 +0530

    Update GetCarrerPlanController.java

--- src/main/java/SpringAIdemo/controller/GetCarrerPlanController.java
+++ src/main/java/SpringAIdemo/controller/GetCarrerPlanController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://career-planner-app-8305041adb2a.herokuapp.com/")
 @RestController
 public class GetCarrerPlanController {
 
