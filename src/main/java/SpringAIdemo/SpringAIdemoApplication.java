5a527ddb12295f347481600d933eb7e1495fe37c
Gourav Thakur <104557560+JavaNerd100@users.noreply.github.com>
2025-05-27 14:02:50 +0530

    Update SpringAIdemoApplication.java

--- src/main/java/SpringAIdemo/SpringAIdemoApplication.java
+++ src/main/java/SpringAIdemo/SpringAIdemoApplication.java
@@ -2,6 +2,8 @@ package SpringAIdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
 
 @SpringBootApplication
 @PropertySource("key.properties")
